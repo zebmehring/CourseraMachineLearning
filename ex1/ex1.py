d5f1ba9603f96df0c9d902c22d672224f586f5c0
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-05-10 23:32:16 -0400

    [ex1] implemented loadData and computeCost methods

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -3,11 +3,27 @@ import matplotlib.pyplot as plt
 
 
 def loadData(filename):
-    pass
+    try:
+        f = open(filename, 'r')
+    except FileNotFoundError:
+        print('{} not found'.format(filename))
+        return []
+    data = None
+    for line in f:
+        ex = np.array([float(i) for i in line.split(',')])
+        if data is not None:
+            data = np.block([[data], [ex]])
+        else:
+            data = ex
+    f.close()
+    return np.block([np.ones((np.shape(data)[0], 1)), data[:, :-1]]), data[:, -1][:, np.newaxis]
 
 
 def computeCost(X, y, theta):
-    pass
+    m = len(y)
+    error = np.dot(X, theta) - y
+    J = np.reciprocal(2.0 * m) * np.dot(np.transpose(error), error)
+    return J.flatten()[0]
 
 
 def featureNormalize(X):
@@ -23,4 +39,11 @@ def normalEquation(X, y):
 
 
 if __name__ == '__main__':
-    pass
+    iterations = 1500
+    alpha = 0.01
+    filename = 'ex1data2.txt'
+
+    X, y = loadData(filename)
+    theta = np.zeros((np.shape(X)[1], 1))
+    J = computeCost(X, y, theta)
+    print('Initial cost: {}'.format(J))
