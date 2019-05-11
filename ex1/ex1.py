f89f0ffdb34c447e6023f3e775823a34b87a2e69
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-05-11 12:53:26 -0400

    [ex1] added comments

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -9,8 +9,8 @@ def loadData(filename):
         filename: string from which to read CSV data
 
     returns: (<float> X, <float> y)
-        X: a numpy 2D matrix where each row is an example and each column is a feature
-        y: a numpy column vector containing the labeled outcomes
+        X: 2D matrix where each row is an example and each column is a feature
+        y: column vector containing the labeled outcomes
 
     Load CSV data from a file into data structures for a regression problem.
     """
@@ -33,9 +33,9 @@ def loadData(filename):
 def computeCost(X, y, theta):
     """
     parameters:
-        X: a numpy 2D matrix where each row is an example and each column is a feature
-        y: a numpy column vector containing the labeled outcomes
-        theta: a numpy column vector containing the hypothesis
+        X: 2D matrix where each row is an example and each column is a feature
+        y: column vector containing the labeled outcomes
+        theta: column vector containing the hypothesis
 
     returns: <float> J
         J: MSE cost for the training batch X with the hypothesis theta
@@ -49,6 +49,17 @@ def computeCost(X, y, theta):
 
 
 def featureNormalize(X):
+    """
+    parameters:
+        X: a 2D matrix where each row is an example and each column is a feature
+
+    returns: <float> (X_norm, mu, sigma)
+        X_norm: 2D matrix where each row is an example and each column is a feature, with all features normalized
+        mu: row vector where each entry is the mean of the feature in the corresponding column of X
+        sigma: row vector where each entry is the standard deviation of the feature in the corresponding column of X
+
+    Normalize the features of the input X.
+    """
     X = X[:, 1:]
     mu = np.mean(X, 0)
     sigma = np.std(X, 0)
@@ -57,6 +68,20 @@ def featureNormalize(X):
 
 
 def gradientDescent(X, y, theta, alpha, iterations):
+    """
+    parameters:
+        X: 2D matrix where each row is an example and each column is a feature
+        y: column vector containing the labeled outcomes
+        theta: column vector containing the hypothesis
+        alpha: learning rate
+        iterations: number of iterations of the algorithm to run
+
+    returns: <float> (theta, J)
+        theta: modified hypothesis after running gradient descent
+        J: column vector storing the cost at each iteration
+
+    Run gradient descent on the hypothesis theta.
+    """
     m = len(y)
     J = np.zeros((iterations, 1))
     for i in range(iterations):
@@ -67,21 +92,34 @@ def gradientDescent(X, y, theta, alpha, iterations):
 
 
 def normalEquation(X, y):
+    """
+    parameters:
+        X: 2D matrix where each row is an example and each column is a feature
+        y: column vector containing the labeled outcomes
+
+    returns: <float> theta
+        theta: optimal hypothesis as computed by the normal equation
+
+    Solves a linear regression problem using the normal equation.
+    """
     return np.dot(np.dot(np.linalg.pinv(np.dot(X.T, X)), X.T), y)
 
 
 def polyFit(x, n):
+    """
+    parameters:
+        x: column vector representing a single feature
+        n: polynomial degree
+
+    returns: <float> X
+        X: 2D matrix whose columns are the original feature to the jth power
+
+    Converts a single feature into a matrix representing a polynomial function of that feature.
+    """
     X = np.ones(x.shape)
     for i in range(1, n + 1):
         X = np.block([[X, np.power(x, i)]])
     return X
-
-
-def plotData(x, y, ax):
-    ax.plot(x, y, 'rx', markersize=10, label=r'Training data')
-    ax.set_ylabel(r'Profit in \$10,000s')
-    ax.set_xlabel(r'Population of City in 10,000s')
-    return ax
 
 
 if __name__ == '__main__':
@@ -93,21 +131,25 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     filename = 'ex1data1.txt'
     X, y = loadData(filename)
-    m = len(y)
-    ax = plotData(np.delete(X, 0, 1), y, ax)
+
+    ax.plot(np.delete(X, 0, 1), y, 'rx', markersize=10, label=r'Training data')
+    ax.set_ylabel(r'Profit in \$10,000s')
+    ax.set_xlabel(r'Population of City in 10,000s')
 
     # Part 1.3: Cost and Gradient Descent
     print('========== Part 1.3: Population Data (GD) ==========')
     theta = np.zeros((X.shape[1], 1))
     optimal = np.array([[-3.6303, 1.1664]]).T
-    iterations = 150
+    iterations = 1500
     alpha = 0.01
+
     J = computeCost(X, y, theta)
     print('Initial cost for population data: {}'.format(J))
     theta, J = gradientDescent(X, y, theta, alpha, iterations)
     print('Final cost for population data: {}'.format(J[-1][0]))
     print('Hypothesis for population data: [{0:0.4f}, {1:0.4f}] (expected: [{2}, {3}])'.format(
         theta[0, 0], theta[1, 0], optimal[0, 0], optimal[1, 0]))
+
     ax.plot(X[:, -1], np.dot(X, theta), 'b-', label='Linear regression')
     ax.legend()
     plt.show()
@@ -138,7 +180,6 @@ if __name__ == '__main__':
     # Part 2.1: Feature Normalization
     filename = 'ex1data2.txt'
     X, y = loadData(filename)
-    m = len(y)
     X, mu, sigma = featureNormalize(X)
 
     # Part 2.2: Gradient Descent
@@ -146,13 +187,16 @@ if __name__ == '__main__':
     theta = np.zeros((X.shape[1], 1))
     iterations = 1500
     alpha = 0.01
+
     J = computeCost(X, y, theta)
     print('Initial cost for housing data: {}'.format(J))
     theta, J = gradientDescent(X, y, theta, alpha, iterations)
+
     plt.plot(np.arange(iterations), J, 'b-', label='Cost')
     plt.xlabel('Iteration')
     plt.ylabel('Cost')
     plt.show()
+
     print('Final cost for housing data: {}'.format(J[-1][0]))
     print('Hypothesis for housing data using gradient descent: [{0:0.4f}, {1:0.4f}, {2:0.4f}]'.format(
         theta[0, 0], theta[1, 0], theta[2, 0]))
