f6a8b59e84614d56fa8a49598cc319f78eb10107
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-05-10 23:31:33 -0400

    [ex1] initial commit

---
+++ ex1/ex1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def loadData(filename):
+    pass
+
+
+def computeCost(X, y, theta):
+    pass
+
+
+def featureNormalize(X):
+    pass
+
+
+def gradientDescent(X, y, theta, alpha, iterations):
+    pass
+
+
+def normalEquation(X, y):
+    pass
+
+
+if __name__ == '__main__':
+    pass
