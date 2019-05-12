01ea3c629606495382162c0c5ade2375fef29dde
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-05-11 23:04:08 -0400

    [ex2] implemented all methods in logisticRegression and finished parts 1.1-1.3

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -17,10 +17,43 @@ def loadData(filename):
         else:
             data = ex
     f.close()
-    return np.block([np.ones((data.shape[0], 1)), data[:, :-1]]), np.array([data[:, -1]]).T
+    return np.block([np.ones((data.shape[0], 1)), data[:, :-1]]), data[:, -1]
 
 
 if __name__ == '__main__':
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     logreg = logisticRegression.LogisticRegression()
+
+    # Part 1.1: Plotting
+    filename = 'ex2data1.txt'
+    X, y = loadData(filename)
+
+    plt.xlabel('Exam 1 Score')
+    plt.ylabel('Exam 2 Score')
+    plt.plot(X[np.where(y == 1)[0], 1], X[np.where(y == 1)[0], 2],
+             'k+', markersize=7, label=r'Admitted')
+    plt.plot(X[np.where(y == 0)[0], 1], X[np.where(y == 0)[0], 2],
+             'ko', markersize=7, label=r'Not admitted')
+
+    # Part 1.2: Compute Cost and Gradient
+    print('========== Parts 1.2-1.3: Cost, Gradient, and Optimization ==========')
+    theta = np.zeros((X.shape[1],))
+    J = logreg.cost(theta, X, y)
+    print('Initial cost for exam data: {0:.3f} (expected 0.693)'.format(J))
+    grad = logreg.cost_grad(theta, X, y)
+    print('Initial gradient for exam data: [{0:.4f}, {1:0.4f}, {2:0.4f}] (expected [-0.1000, -12.0092, -11.2628])'.format(
+        grad[0], grad[1], grad[2]))
+
+    # Part 1.3: Optimization
+    J, theta = logreg.optimize(theta, X, y)
+    print('Final cost for exam data: {0:.3f} (expected 0.203)'.format(J))
+    print('Hypothesis for exam data: [{0:.3f}, {1:0.3f}, {2:0.3f}] (expected [-25.161, 0.206, 0.201])'.format(
+        theta[0], theta[1], theta[2]))
+
+    plot_x = np.array([min(X[:, 1]) - 2, max(X[:, 1]) + 2])
+    plot_y = (-1 / theta[2]) * (theta[1] * plot_x + theta[0])
+    plt.plot(plot_x, plot_y, 'k-', label='Decision boundary')
+    plt.legend()
+    plt.show()
+    plt.clf()
--- ex2/logisticRegression.py
+++ ex2/logisticRegression.py
@@ -1,13 +1,26 @@
 import numpy as np
-import scipy.optimize as op
+from scipy.optimize import minimize
 
 
 class LogisticRegression:
-    def costFunction(self, X, y, theta, l=0):
-        pass
+    def cost(self, theta, X, y):
+        np.seterr(divide='ignore', invalid='ignore')
+        g = self.sigmoid(X @ theta)
+        return - (1 / len(y)) * sum((np.log(g).T * y) +
+                                    (np.log(1 - g).T * (1 - y)))
 
-    def predict(self, X, theta):
-        pass
+    def predict(self, theta, X):
+        return np.where(X @ theta < 0, 0, 1)
 
     def sigmoid(self, z):
-        pass
+        return np.reciprocal(1 + np.exp(-z))
+
+    def cost_grad(self, theta, X, y):
+        return (1 / len(y)) * X.T @ (self.sigmoid(X @ theta) - y)
+
+    def optimize(self, theta, X, y):
+        res = minimize(self.cost, theta, (X, y), method='BFGS',
+                       jac=self.cost_grad, options={'maxiter': 400})
+        theta = res.x
+        J = self.cost(theta, X, y)
+        return J, theta
