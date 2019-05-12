f6b20c08b395ae38ad297208758ac77316ce8453
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-05-12 16:38:15 -0400

    [ex2] implemented regularization and the rest of main

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import logisticRegression
 
 
-def loadData(filename):
+def load_data(filename):
     try:
         f = open(filename, 'r')
     except FileNotFoundError:
@@ -20,6 +20,16 @@ def loadData(filename):
     return np.block([np.ones((data.shape[0], 1)), data[:, :-1]]), data[:, -1]
 
 
+def map_feature(X1, X2, degree):
+    X1 = X1.reshape((len(X1), 1))
+    X2 = X2.reshape((len(X2), 1))
+    X = np.ones((X1.shape[0], 1))
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            X = np.block([X, np.power(X1, i - j) * np.power(X2, j)])
+    return X
+
+
 if __name__ == '__main__':
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
@@ -27,7 +37,7 @@ if __name__ == '__main__':
 
     # Part 1.1: Plotting
     filename = 'ex2data1.txt'
-    X, y = loadData(filename)
+    X, y = load_data(filename)
 
     plt.xlabel('Exam 1 Score')
     plt.ylabel('Exam 2 Score')
@@ -60,5 +70,44 @@ if __name__ == '__main__':
 
     # Part 1.4: Predict
     p = logreg.predict(theta, X)
-    print('Testing accuracy: {0:.1f}% (expected 89.0%)'.format(
-        np.mean(p == y) * 100))
+    print('Accuracy: {0:.1f}% (expected 89.0%)'.format(np.mean(p == y) * 100))
+
+    # Part 2.1: Plotting with Regularization
+    filename = 'ex2data2.txt'
+    X, y = load_data(filename)
+
+    plt.xlabel('Microchip Test 1')
+    plt.ylabel('Microchip Test 2')
+    plt.plot(X[np.where(y == 1)[0], 1], X[np.where(y == 1)[0], 2],
+             'k+', markersize=7, label=r'$y=1$')
+    plt.plot(X[np.where(y == 0)[0], 1], X[np.where(y == 0)[0], 2],
+             'ko', markersize=7, label=r'$y=0$')
+
+    # Part 2.2: Feature Mapping
+    X = map_feature(X[:, 1], X[:, 2], 6)
+
+    # Part 2.3: Cost and Gradient
+    print('========== Part 2: Regularization ==========')
+    theta = np.zeros((X.shape[1],))
+    reg = 1
+    J = logreg.cost(theta, X, y, reg)
+    print('Initial cost for chip data: {0:.3f} (expected 0.693)'.format(J))
+    J, theta = logreg.optimize(theta, X, y, reg)
+    print('Final cost for chip data: {0:.3f}'.format(J))
+
+    u = np.linspace(-1, 1.5, 50)
+    v = np.linspace(-1, 1.5, 50)
+    z = np.zeros((len(u), len(v)))
+    for i in range(len(u)):
+        for j in range(len(v)):
+            z[i, j] = map_feature(
+                np.array([u[i]]), np.array([v[j]]), 6) @ theta
+    z = z.T
+
+    plt.contour(u, v, z, 0)
+    plt.legend()
+    plt.show()
+
+    # Part 2.4: Predict
+    p = logreg.predict(theta, X)
+    print('Accuracy: {0:.1f}% (expected 83.1%)'.format(np.mean(p == y) * 100))
--- ex2/logisticRegression.py
+++ ex2/logisticRegression.py
@@ -3,11 +3,11 @@ from scipy.optimize import minimize
 
 
 class LogisticRegression:
-    def cost(self, theta, X, y):
+    def cost(self, theta, X, y, reg=0):
         np.seterr(divide='ignore', invalid='ignore')
-        g = self.sigmoid(X @ theta)
-        return - (1 / len(y)) * sum((np.log(g).T * y) +
-                                    (np.log(1 - g).T * (1 - y)))
+        m = len(y)
+        h = self.sigmoid(X @ theta)
+        return (- (1 / m) * ((y.T @ np.log(h)) + ((1 - y).T @ np.log(1 - h)))) + ((reg / (2 * m)) * (theta[1:].T @ theta[1:]))
 
     def predict(self, theta, X):
         return np.where(X @ theta < 0, 0, 1)
@@ -15,11 +15,13 @@ class LogisticRegression:
     def sigmoid(self, z):
         return np.reciprocal(1 + np.exp(-z))
 
-    def cost_grad(self, theta, X, y):
-        return (1 / len(y)) * X.T @ (self.sigmoid(X @ theta) - y)
+    def cost_grad(self, theta, X, y, reg=0):
+        m = len(y)
+        h = self.sigmoid(X @ theta)
+        return (1 / m) * ((h - y) @ X) + (reg / m) * np.block([0, theta[1:]])
 
-    def optimize(self, theta, X, y):
-        res = minimize(self.cost, theta, (X, y), method='BFGS',
+    def optimize(self, theta, X, y, reg=0):
+        res = minimize(self.cost, theta, (X, y, reg), method='BFGS',
                        jac=self.cost_grad, options={'maxiter': 400})
         theta = res.x
         J = self.cost(theta, X, y)
