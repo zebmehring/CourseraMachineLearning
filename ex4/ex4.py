7385c4521ef046981a028981ab6bebd8323e2e46
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-06-01 12:33:26 -1000

    [ex4] preliminary implementation of backprop -- scipy.optimize.minimize hangs

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -5,43 +5,67 @@ import neuralNetwork
 
 
 if __name__ == '__main__':
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    nn = neuralNetwork.NeuralNetwork()
-
     layer_dims = [400, 25, 10]
     data = scipy.io.loadmat('ex4data1.mat')
     X, y = (data['X'], data['y'])
-    m = y.shape[0]
-    y = np.where(y == 10, 0, y).reshape(m)
+    y = np.where(y == 10, 0, y).reshape(y.size).astype(float)
     data = scipy.io.loadmat('ex4weights.mat')
     Theta = [data['Theta1'], data['Theta2']]
     theta = np.block([t.reshape(t.size, order='F') for t in Theta])
+    nn = neuralNetwork.NeuralNetwork(X, y, Theta, layer_dims, 0)
 
-    J = nn.cost(theta, X, y, layer_dims)
+    print('========== Part 1.3: Feedforward and Cost Function ==========')
+    J = nn.cost(theta)
     print('Cost: {0:0.6f} (expected: 0.287629)'.format(J))
 
-    J = nn.cost(theta, X, y, layer_dims, 1)
+    print('========== Part 1.4: Regularized Cost Function ==========')
+    nn.update_lambda(1)
+    J = nn.cost(theta)
     print('Regularized cost: {0:0.6f} (expected: 0.383770)'.format(J))
 
+    print('========== Part 2.1: Sigmoid Gradient ==========')
     g = nn.sigmoid_gradient(np.array([-1.0, -0.5, 0, 0.5, 1.0]))
-    print('Sigmoid gradient: [{0:0.6f}, {1:0.6f}, {2:0.6f}, {3:0.6f}, {4:0.6f}] (expected: [0.19661193, 0.23500371, 0.250000, 0.23500371, 0.19661193])'.format(
-        g[0], g[1], g[2], g[3], g[4]))
+    e_g = [0.19661193, 0.23500371, 0.250000, 0.23500371, 0.19661193]
+    print('---------------------')
+    print('   Actual |  Expected')
+    print('---------------------')
+    for i, j in zip(g, e_g):
+        print('{0: .6f} | {1: .6f}'.format(i, j))
+    diff = np.linalg.norm(e_g - g) / np.linalg.norm(e_g + g)
+    print('Relative difference: {0}'.format(diff))
 
+    print('========== Part 2.4: Gradient Checking ==========')
     sample_dims = [3, 5, 3]
     sample_m = 5
-    X_sample = nn.initialize_weights(
-        sample_dims[0] - 1, sample_m, None, debug=True)
-    y_sample = 1 + (np.arange(1, sample_m + 1) % sample_dims[-1])
+    X_s = nn.initialize_weights(sample_dims[0] - 1, sample_m, None, debug=True)
+    y_s = 1 + (np.arange(1, sample_m + 1) % sample_dims[-1])
     Theta = [nn.initialize_weights(sample_dims[i], sample_dims[i + 1], None, debug=True)
              for i in range(len(sample_dims) - 1)]
     theta = np.block([t.reshape(t.size, order='F') for t in Theta])
-    grads = nn.cost_grad(theta, X_sample, y_sample, sample_dims)
-    n_grads = nn.cost_grad_numerical(theta, X_sample, y_sample, sample_dims)
-    print('Relative difference in analytic vs. numerical gradients: {0}'.format(
-        np.linalg.norm(n_grads - grads) / np.linalg.norm(n_grads + grads)))
+    sample_nn = neuralNetwork.NeuralNetwork(X_s, y_s, Theta, sample_dims)
 
+    grads = sample_nn.cost_grad(theta)
+    n_grads = sample_nn.cost_grad_numerical(theta)
+    print('----------------------')
+    print('Analytical | Numerical')
+    print('----------------------')
+    for g, n_g in zip(grads, n_grads):
+        print('   {0: .4f} |   {1: .4f}'.format(g, n_g))
+    diff = np.linalg.norm(n_grads - grads) / np.linalg.norm(n_grads + grads)
+    print('Relative difference: {0}'.format(diff))
+
+    print('========== Part 2.4: Regularized Neural Networks ==========')
     Theta = [data['Theta1'], data['Theta2']]
     theta = np.block([t.reshape(t.size, order='F') for t in Theta])
-    J = nn.cost(theta, X, y, layer_dims, 3)
+    nn.update_lambda(3)
+    J = nn.cost(theta)
     print('Regularized cost: {0:0.6f} (expected: 0.576051)'.format(J))
+
+    print('========== Part 2.5: Learning Parameters ==========')
+    Theta = [nn.initialize_weights(layer_dims[i], layer_dims[i + 1], 0.12)
+             for i in range(len(layer_dims) - 1)]
+    theta = np.block([t.reshape(t.size, order='F') for t in Theta])
+    nn.update_lambda(1)
+    J, theta = nn.optimize(theta)
+    p = nn.predict(theta)
+    print('Trained accuracy: {}'.format(np.mean(p == y) * 100))
--- ex4/neuralNetwork.py
+++ ex4/neuralNetwork.py
@@ -3,26 +3,51 @@ from scipy.optimize import minimize
 
 
 class NeuralNetwork:
-    def cost(self, theta, X, y, layer_dims, l=0, reorder=True):
+    def __init__(self, X, y, Theta, layer_dims, l=0):
         """
         parameters:
             <mxn> X: 2D matrix where each row is an example and each column is a feature
             <mx1> y: 1D vector containing the labeled outcomes
             [<axb>] Theta: list of 2D matricies containing weights for the neural network layers
             [<int>] layer_dims: list representing the size of each layer in the network
-            <float> reg: regularization parameter (default 0)
+            <float> l: regularization parameter (default 0)
+        """
+        self.X = X
+        self.y = y
+        self.m = len(y)
+        self.Theta = Theta
+        self.layer_dims = layer_dims
+        self.l = l
+        self.iterations = 1
+
+    def update_weights(self, Theta):
+        self.Theta = Theta
+
+    def update_architecture(self, layer_dims):
+        self.layer_dims = layer_dims
+
+    def update_training_set(self, X, y):
+        self.X = X
+        self.y = y
+        self.m = len(y)
+
+    def update_lambda(self, l):
+        self.l = l
+
+    def cost(self, theta, reorder=True):
+        """
+        parameters:
 
         returns:
             <float> J: cost for the neural network with parameters Theta using the data from X
 
         Compute the regularized cost for the data in X with the nerual network specified by Theta.
         """
-        np.seterr(divide='ignore', invalid='ignore')  # ignore log(0) errors
-        m = len(y)
-        Theta = self.reconstruct_Theta(theta, layer_dims)
+        # np.seterr(divide='ignore', invalid='ignore')  # ignore log(0) errors
+        Theta = self.reconstruct_Theta(theta)
 
         # feedforward and compute the output
-        a = X
+        a = self.X
         for t in Theta:
             a = self.sigmoid(np.block([np.ones((a.shape[0], 1)), a]) @ t.T)
         # reorder columns for 0-based indexing
@@ -31,8 +56,8 @@ class NeuralNetwork:
 
         # compute the summed cost for each label
         cost = 0
-        for k in range(layer_dims[-1]):
-            y_k = np.where(y == k, 1, 0)
+        for k in range(self.layer_dims[-1]):
+            y_k = np.where(self.y == k, 1, 0)
             h_k = a[:, k]
             cost += (y_k.T @ np.log(h_k)) + ((1 - y_k).T @ np.log(1 - h_k))
 
@@ -40,9 +65,9 @@ class NeuralNetwork:
         reg = sum([sum(sum(np.square(t[:, 1:]))) for t in Theta])
 
         # compute the total cost
-        return -(1 / m) * cost + (l / (2 * m)) * reg
+        return -(1 / self.m) * cost + (self.l / (2 * self.m)) * reg
 
-    def cost_grad(self, theta, X, y, layer_dims, l=0):
+    def cost_grad(self, theta):
         """
         parameters:
 
@@ -52,17 +77,16 @@ class NeuralNetwork:
 
 
         """
-        m = len(y)
-        Theta = self.reconstruct_Theta(theta, layer_dims)
+        Theta = self.reconstruct_Theta(theta)
 
         grads = [np.zeros(Theta[i].shape) for i in range(len(Theta))]
-        for t in range(m):
-            a = [X[t, :]]
+        for t in range(self.m):
+            a = [self.X[t, :]]
             z = [None]
             for i in range(len(Theta)):
                 z.append(Theta[i] @ np.block([1, a[i]]))
                 a.append(self.sigmoid(z[-1]))
-            y_t = np.where(np.arange(layer_dims[-1]) == y[t], 1, 0)
+            y_t = np.where(np.arange(self.layer_dims[-1]) == self.y[t], 1, 0)
 
             delta = [a[-1] - y_t]
             for i in range(len(a) - 2, 0, -1):
@@ -74,12 +98,12 @@ class NeuralNetwork:
                 grads[i] = grads[i] + np.outer(delta[i], np.block([1, a[i]]))
 
         for i in range(len(Theta)):
-            grads[i] = (1 / m) * grads[i] + (l / m) * \
+            grads[i] = (1 / self.m) * grads[i] + (self.l / self.m) * \
                 np.block([np.zeros((Theta[i].shape[0], 1)), Theta[i][:, 1:]])
 
         return np.block([g.reshape(g.size, order='F') for g in grads])
 
-    def cost_grad_numerical(self, theta, X, y, layer_dims, l=0):
+    def cost_grad_numerical(self, theta):
         """
         """
         grads = np.zeros(theta.size)
@@ -87,15 +111,13 @@ class NeuralNetwork:
         e = 1e-4
         for p in range(theta.size):
             perturbation[p] = e
-            loss1 = self.cost(theta - perturbation, X, y,
-                              layer_dims, l, reorder=False)
-            loss2 = self.cost(theta + perturbation, X, y,
-                              layer_dims, l, reorder=False)
+            loss1 = self.cost(theta - perturbation, reorder=False)
+            loss2 = self.cost(theta + perturbation, reorder=False)
             grads[p] = (loss2 - loss1) / (2 * e)
             perturbation[p] = 0
         return grads
 
-    def predict(self, Theta, X):
+    def predict(self, theta):
         """
         parameters:
             [<axb>] Theta: list of 2D matricies containing weights for the neural network layers
@@ -106,9 +128,11 @@ class NeuralNetwork:
 
         Compute the output of a neural network using the weights provided. The architecture is specified by the dimensions of these weights.
         """
-        for theta in Theta:
-            a = self.sigmoid(np.block([np.ones((a.shape[0], 1)), a]) @ theta.T)
-        return (np.argmax(a, 1) + 1) % Theta[-1].shape[1]
+        Theta = self.reconstruct_Theta(theta)
+        a = self.X
+        for t in Theta:
+            a = self.sigmoid(np.block([np.ones((a.shape[0], 1)), a]) @ t.T)
+        return np.argmax(a, 1)
 
     def sigmoid(self, z):
         """
@@ -143,18 +167,25 @@ class NeuralNetwork:
         else:
             return 2 * np.random.rand(n_out, 1 + n_in) * epsilon_init - epsilon_init
 
-    def reconstruct_Theta(self, theta, layer_dims):
+    def reconstruct_Theta(self, theta):
         """
         """
         Theta = []
         t_i = 0
-        for i in range(len(layer_dims) - 1):
-            Theta.append(np.reshape(
-                theta[t_i:t_i+((layer_dims[i] + 1) * layer_dims[i+1])], (layer_dims[i+1], layer_dims[i]+1), order='F'))
-            t_i = (layer_dims[i] + 1) * layer_dims[i+1]
+        for i in range(len(self.layer_dims) - 1):
+            Theta.append(np.reshape(theta[t_i:t_i+((self.layer_dims[i] + 1) * self.layer_dims[i + 1])],
+                                    (self.layer_dims[i + 1], self.layer_dims[i] + 1), order='F'))
+            t_i = (self.layer_dims[i] + 1) * self.layer_dims[i + 1]
         return Theta
 
-    def optimize(self, theta, X, y, layer_dims, l=0):
+    def print_cost(self, theta):
+        """
+        """
+        cost = self.cost(theta)
+        print("Iteration {0}: {1}".format(self.iterations, cost))
+        self.iterations += 1
+
+    def optimize(self, theta):
         """
         parameters:
 
@@ -164,8 +195,9 @@ class NeuralNetwork:
 
 
         """
-        res = minimize(self.cost, theta, (X, y, layer_dims, l), method='CG',
-                       jac=self.cost_grad, options={'maxiter': 50})
+        self.iterations = 1
+        res = minimize(self.cost, theta, method='CG', callback=self.print_cost,
+                       jac=self.cost_grad, options={'maxiter': 50, 'disp': True})
         theta = res.x
-        J = self.cost(theta, X, y, layer_dims)
+        J = self.cost(theta)
         return J, theta
