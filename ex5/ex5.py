f27444604f62aae16c11c5899da367363068fbf0
Zeb Mehring <zeb.mehring+github@gmail.com>
2019-06-03 13:20:52 -1000

    [ex5] implemented all parts

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -1,14 +1,188 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io
+from scipy.io import loadmat
 from linearRegression import LinearRegression
 
 
+def learning_curve(X, y, X_val, y_val, l=0, randomize=False, iters=1):
+    linreg = LinearRegression(X, y, l)
+    linreg_val = LinearRegression(X_val, y_val, 0)
+    X = linreg.poly_fit(8)
+    X = linreg.feature_normalize()
+    X_val = linreg_val.poly_fit(8)
+    X_val = linreg_val.feature_normalize()
+    m_val = X_val.shape[0]
+
+    error_train = np.zeros(linreg.m)
+    error_val = np.zeros(linreg.m)
+
+    for n in range(linreg.m):
+        e_t = np.zeros(iters)
+        e_v = np.zeros(iters)
+        for i in range(iters):
+            if randomize:
+                train_i = [np.random.randint(linreg.m) for _ in range(n + 1)]
+                val_i = [np.random.randint(linreg_val.m) for _ in range(n + 1)]
+            else:
+                train_i = np.arange(n + 1)
+                val_i = np.arange(m_val)
+            linreg.update_training_set(X[train_i, :], y[train_i])
+            linreg_val.update_training_set(X_val[val_i, :], y_val[val_i])
+            _, theta = linreg.optimize()
+            e_t[i] = linreg.cost(theta, l=0)
+            e_v[i] = linreg_val.cost(theta, l=0)
+        error_train[n] = np.mean(e_t)
+        error_val[n] = np.mean(e_v)
+
+    return error_train, error_val
+
+
+def validation_curve(X, y, X_val, y_val, lambdas):
+    linreg = LinearRegression(X, y)
+    linreg_val = LinearRegression(X_val, y_val)
+    X = linreg.poly_fit(8)
+    X = linreg.feature_normalize()
+    X_val = linreg_val.poly_fit(8)
+    X_val = linreg_val.feature_normalize()
+
+    error_train = np.zeros(len(lambdas))
+    error_val = np.zeros(len(lambdas))
+
+    for i in range(len(lambdas)):
+        linreg.update_lambda(lambdas[i])
+        _, theta = linreg.optimize()
+        error_train[i] = linreg.cost(theta, l=0)
+        error_val[i] = linreg_val.cost(theta, l=0)
+
+    return error_train, error_val
+
+
 if __name__ == '__main__':
-    data = scipy.io.loadmat('ex5data1.mat')
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
+    # ==================== Part 1.1 Dataset ====================
+    data = loadmat('ex5data1.mat')
     X, y = (data['X'], data['y'])
     X_val, y_val = (data['Xval'], data['yval'])
     X_test, y_test = (data['Xtest'], data['ytest'])
     linreg = LinearRegression(X, y, 0)
+    linreg_val = LinearRegression(X_val, y_val, 0)
+    linreg_test = LinearRegression(X_test, y_test, 0)
+    regs = [linreg, linreg_val, linreg_test]
 
-    print('========== Part 1 ==========')
+    # ========== Part 1.2 Regularized Linear Regression Cost ==========
+    theta = np.array([1.0, 1.0])
+    linreg.update_lambda(1)
+    J = linreg.cost(theta)
+    assert np.round(J, decimals=3) == 303.993
+
+    # ========== Part 1.3 Regularized Linear Regression Gradient ==========
+    grad = linreg.cost_grad(theta)
+    assert np.array_equal(np.round(grad, decimals=3),
+                          np.array([-15.303, 598.251]))
+
+    # ========== Part 1.3 Training Linear Regression ==========
+    linreg.update_lambda(0)
+    J, theta = linreg.optimize()
+    J_star, theta_star = linreg.normal_equation()
+    theta = [np.round(t, decimals=3) for t in theta]
+    theta_star = [np.round(t, decimals=3) for t in theta_star]
+    assert np.array_equal(theta, theta_star)
+
+    plt.plot(X, y, 'rx')
+    plt.plot(X, linreg.predict(theta), 'b-', label=r'$\lambda = 0$')
+    plt.xlabel('Change in water level')
+    plt.ylabel('Outgoing water flow')
+    plt.legend()
+    plt.show()
+
+    # ========== Part 2.1 Learning Curves (Linear Regression) ==========
+    error_train, error_val = learning_curve(X, y, X_val, y_val, l=0)
+
+    plt.plot(np.arange(error_val.size), error_train, label='Training error')
+    plt.plot(np.arange(error_val.size), error_val, label='Validation error')
+    plt.xlabel('Number of training examples')
+    plt.ylabel(r'$J(\theta)$')
+    plt.legend()
+    plt.show()
+
+    # ==================== Part 2.2 Feature Mapping ====================
+    for r in regs:
+        r.poly_fit(8)
+        r.feature_normalize()
+
+    # ========== Part 3.1-3.2 Learning Curves (Polynomial Regression) ==========
+    lambdas = [0, 1, 100]
+    for l in lambdas:
+        for r in regs:
+            r.update_lambda(l)
+        J, theta = linreg.optimize()
+        x = np.arange(min(X) - 15, max(X) + 25, 0.05)
+        z = np.polyfit(X.flatten(), linreg.predict(theta), 8)
+        f = np.poly1d(z)
+
+        plt.plot(X, y, 'rx', label='Actual value')
+        plt.plot(X, linreg.predict(theta), 'g+', label='Predicted value')
+        plt.plot(x, f(x), 'b--',
+                 label=r'Predicted trend for $p=8,\ \lambda={0}$'.format(l))
+        plt.xlabel('Change in water level')
+        plt.ylabel('Outgoing water flow')
+        plt.legend()
+        plt.show()
+
+        error_train, error_val = learning_curve(X, y, X_val, y_val, l)
+        print('================== lambda = {0} =================='.format(l))
+        print('# of Examples\tTrain Error\tValidation Error')
+        for i in range(linreg.m):
+            print('{0}\t\t{1:.6f}\t{2:.6f}'.format(
+                i + 1, error_train[i], error_val[i]))
+        print('')
+
+        plt.plot(np.arange(error_val.size),
+                 error_train, label='Training error')
+        plt.plot(np.arange(error_val.size),
+                 error_val, label='Validation error')
+        plt.xlabel('Number of training examples')
+        plt.ylabel(r'$J(\theta)$')
+        plt.legend()
+        plt.show()
+
+    # ==================== Part 3.3 Selecting Lambda ====================
+    lambdas = [0.0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0]
+    error_train, error_val = validation_curve(X, y, X_val, y_val, lambdas)
+    print('============ Selecting Lambda ============')
+    print('lambda\tTrain Error\tValidation Error')
+    for i in range(len(lambdas)):
+        print('{0:.3f}\t{1:.6f}\t{2:.6f}'.format(
+            lambdas[i], error_train[i], error_val[i]))
+    print('')
+
+    plt.plot(np.arange(error_val.size), error_train, label='Training error')
+    plt.plot(np.arange(error_val.size), error_val, label='Validation error')
+    plt.xlabel(r'$\lambda$')
+    plt.ylabel(r'$J(\theta)$')
+    plt.legend()
+    plt.show()
+
+    # ==================== Part 3.4 Test Set Error ====================
+    for r in regs:
+        r.update_lambda(3)
+    J, theta = linreg.optimize()
+    J_val = linreg_val.cost(theta)
+    J_test = linreg_test.cost(theta)
+    print('Cost on validation set: {0}'.format(J_val))
+    print('Cost on test set: {0}'.format(J_test))
+
+    # =============== Part 3.5 Randomized Learning Curves ===============
+    error_train, error_val = learning_curve(
+        X, y, X_val, y_val, l=0.01, randomize=True, iters=50)
+
+    plt.plot(np.arange(error_val.size),
+             error_train, label='Training error')
+    plt.plot(np.arange(error_val.size),
+             error_val, label='Validation error')
+    plt.xlabel('Number of training examples')
+    plt.ylabel(r'$J(\theta)$')
+    plt.legend()
+    plt.show()
--- ex5/linearRegression.py
+++ ex5/linearRegression.py
@@ -11,37 +11,48 @@ class LinearRegression:
             <float> l: regularization parameter
         """
         self.X = X
-        self.y = y
-        self.m, self.n = X.shape
+        self.y = y.flatten()
+        self.m = X.shape[0]
+        self.n = X.shape[1] + 1
         self.l = l
 
     def update_training_set(self, X, y):
         self.X = X
-        self.y = y
-        self.m, self.n = X.shape
+        self.y = y.flatten()
+        self.m = X.shape[0]
+        self.n = X.shape[1] + 1
 
     def update_lambda(self, l):
         self.l = l
 
-    def feature_normalize(self, X):
+    def feature_normalize(self):
         """
+        returns:
+            <mxn> X: normalized feature matrix
+
         Normalize the features of the input X.
         """
         mu = np.mean(self.X, 0)
         sigma = np.std(self.X, 0)
-        self.X = np.c_[np.ones((self.m, 1)), (self.X - mu) / sigma]
+        self.X = (self.X - mu) / sigma
+        return self.X
 
     def poly_fit(self, p):
         """
         parameters:
             <int> p: maximum polynomial degree
 
+        returns:
+            <mxp> X: polynomial feature matrix
+
         Convert a single feature into a polynomial function of that feature, of maximum degree p.
         """
-        assert self.n == 1
-        self.X = np.c_[[np.power(self.X, i) for i in range(1, p + 1)]].T
+        assert self.n == 2
+        self.X = np.block([np.power(self.X, i) for i in range(1, p + 1)])
+        self.n = p + 1
+        return self.X
 
-    def cost(self, theta):
+    def cost(self, theta, l=None):
         """
         parameters:
             <nx1> theta: column vector containing the hypothesis
@@ -51,10 +62,13 @@ class LinearRegression:
 
         Compute the mean-squared-error for the data in X with the hypothesis theta.
         """
-        h = self.X @ theta
+        if l is None:
+            l = self.l
+        X = np.c_[np.ones((self.m, 1)), self.X]
+        h = X @ theta
         err = h - self.y
         reg = (theta[1:].T @ theta[1:])
-        return (1 / (2 * self.m)) * (err.T @ err) + (self.l / (2 * self.m)) * reg
+        return (1 / (2 * self.m)) * (err.T @ err) + (l / (2 * self.m)) * reg
 
     def cost_grad(self, theta):
         """
@@ -66,19 +80,24 @@ class LinearRegression:
 
         Evaluate the gradient of the hypothesis theta on the data from X.
         """
-        h = self.X @ theta
-        err = self.X.T @ (h - self.y)
+        X = np.c_[np.ones((self.m, 1)), self.X]
+        h = X @ theta
+        err = X.T @ (h - self.y)
         reg = np.block([0, theta[1:]])
         return (1 / self.m) * err + (self.l / self.m) * reg
 
     def normal_equation(self):
         """
         returns:
+            <float> J: cost for optimal hypothesis
             <nx1> theta: optimal hypothesis as computed by the normal equation
 
         Solves a linear regression problem using the normal equation.
         """
-        return np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.y
+        X = np.c_[np.ones((self.m, 1)), self.X]
+        theta = np.linalg.pinv(X.T @ X) @ X.T @ self.y
+        J = self.cost(theta)
+        return J, theta
 
     def optimize(self):
         """
@@ -89,9 +108,18 @@ class LinearRegression:
         Solves a linear regression problem using BFGS optimization.
         """
         theta_0 = np.ones((self.n, 1))
-        options = {'maxiter': 200, 'disp': True}
+        options = {'maxiter': 200}
         res = minimize(self.cost, theta_0, method='BFGS',
                        jac=self.cost_grad, options=options)
         theta = res.x
         J = self.cost(theta)
         return J, theta
+
+    def predict(self, theta):
+        """
+        parameters:
+            <nx1> theta: hypothesis vector
+
+        Predict outcomes for the data in X using the hypothesis theta.
+        """
+        return np.c_[np.ones((self.m, 1)), self.X] @ theta
